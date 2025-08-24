4defb0713286b4dfe6994478df755dca2485adf1
Chinmay Ingle <chinmayingle26@gmail.com>
2025-08-24 12:53:49 +0000

    Error: The default Firebase app does not exist. Make sure you call initi

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,20 +1,20 @@
 
 import * as admin from 'firebase-admin';
+import { config } from 'dotenv';
+
+// Load environment variables from .env file
+config();
 
 if (!admin.apps.length) {
-  try {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        // The private key must be formatted correctly.
-        // In the .env file, it should be enclosed in quotes and have newlines represented as \\n
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      }),
-    });
-  } catch (error: any) {
-    console.error('Firebase admin initialization error', error.stack);
-  }
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      // The private key must be formatted correctly.
+      // In the .env file, it should be enclosed in quotes and have newlines represented as \\n
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    }),
+  });
 }
 
 export const adminDb = admin.firestore();
